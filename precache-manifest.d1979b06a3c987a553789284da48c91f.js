1303a5bd56d7cdeba601d5376802ecd855c369af
Patrick Souza <patrick_ams@hotmail.com>
2022-11-24 21:38:03 -0300

    Updates

---
+++ precache-manifest.d1979b06a3c987a553789284da48c91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "858a0d5587bec444a15b",
+    "url": "/city-news-searcher-frontend/static/css/main.d6050904.chunk.css"
+  },
+  {
+    "revision": "858a0d5587bec444a15b",
+    "url": "/city-news-searcher-frontend/static/js/main.d5e7f3fd.chunk.js"
+  },
+  {
+    "revision": "2b97c5a9c7cffa5e2e98",
+    "url": "/city-news-searcher-frontend/static/js/runtime~main.e713f161.js"
+  },
+  {
+    "revision": "b8e66a1a9a91fb54b0f3",
+    "url": "/city-news-searcher-frontend/static/js/2.001214cc.chunk.js"
+  },
+  {
+    "revision": "60cf4ecf76dd825d6b0e",
+    "url": "/city-news-searcher-frontend/static/js/3.b84871a5.chunk.js"
+  },
+  {
+    "revision": "92fcbce809320497e69fc864354484a1",
+    "url": "/city-news-searcher-frontend/index.html"
+  }
+];
