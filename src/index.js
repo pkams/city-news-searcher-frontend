fda1ffc830045a2cb2496e7f323837f0b19611f8
Patrick Souza <patrick_ams@hotmail.com>
2022-11-24 22:28:21 -0300

    update basename on router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
